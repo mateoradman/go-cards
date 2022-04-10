8a6a74c24bf383b0b26af624b72dfe84519c50b8
Mateo Radman <48420316+mateoradman@users.noreply.github.com>
2022-04-10 10:23:59 +0200

    Add endpoints for creating, getting, and drawing cards from deck.

--- main.go
+++ main.go
@@ -3,13 +3,108 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
+	"strconv"
+	"strings"
 
-	"github.com/gorilla/mux"
+	"github.com/julienschmidt/httprouter"
+	"github.com/mateoradman/go-cards/internal/models"
 )
 
-func main() {
-	router := mux.NewRouter()
+type application struct {
+	logger *log.Logger
+}
 
-	log.Println("Starting server on port 8080")
-	http.ListenAndServe(":8080", router)
+var InMemoryStorage = make(map[string]*models.Deck)
+
+func main() {
+	// Initialize a new logger which writes messages to the standard out stream,
+	// prefixed with the current date and time.
+	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
+
+	app := &application{logger: logger}
+	srv := &http.Server{
+		Addr:    ":8080",
+		Handler: app.routes(),
+	}
+
+	logger.Printf("Starting server on localhost%s", srv.Addr)
+	logger.Fatal(srv.ListenAndServe())
+}
+
+func (app *application) routes() *httprouter.Router {
+	router := httprouter.New()
+	router.GET("/api/v1/decks/:uuid", app.getDeck)
+	router.POST("/api/v1/decks/:uuid/draw", app.drawCardFromDeck)
+	router.POST("/api/v1/decks", app.createDeck)
+	return router
+}
+
+func (app *application) createDeck(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	shuffledQuery := r.URL.Query().Get("shuffled")
+	cardsQuery := r.URL.Query().Get("cards")
+	var cards []models.Card
+	if cardsQuery == "" {
+		cards = models.GetAllCards()
+	} else {
+		// split cards string and create new card for each
+		cardCodes := strings.Split(cardsQuery, ",")
+		cards = models.NewCardsFromCodes(cardCodes)
+	}
+	deck := models.NewDeck(cards)
+
+	if shuffledQuery == "true" {
+		deck.Shuffle()
+	}
+	// Add deck to in memory storage
+	InMemoryStorage[deck.Deck_ID] = &deck
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	// Convert the deck to JSON
+	minimalDeck := models.MinimalDeck{Deck_ID: deck.Deck_ID, Shuffled: deck.Shuffled, Remaining: deck.Remaining}
+	w.Write(minimalDeck.ToJSON())
+}
+
+func (app *application) getDeck(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	uuid := ps.ByName("uuid")
+	deck, ok := InMemoryStorage[uuid]
+	if !ok {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(deck.ToJSON())
+}
+
+func (app *application) drawCardFromDeck(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	uuid := ps.ByName("uuid")
+	deck, ok := InMemoryStorage[uuid]
+	if !ok {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+	// Validate count query param.
+	count := r.URL.Query().Get("count")
+	if count == "" {
+		w.Write([]byte("Please specify the number of cards to draw."))
+		return
+	}
+	countInt, err := strconv.Atoi(count)
+	if err != nil {
+		w.Write([]byte("Please specify the number of cards to draw as an integer."))
+		return
+	}
+	if countInt > deck.Remaining {
+		w.Write([]byte("Not enough cards in the deck."))
+		return
+	}
+
+	drawnCards := deck.Draw(countInt)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+
+	// Convert the cards to JSON
+	deckCards := models.DeckCards{Cards: drawnCards}
+	w.Write(deckCards.ToJSON())
 }
--- main_test.go
+++ main_test.go
@@ -1,1 +1,159 @@
 package main
+
+import (
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/julienschmidt/httprouter"
+	"github.com/mateoradman/go-cards/internal/models"
+)
+
+func createNewCompleteDeck() models.Deck {
+	allCards := models.GetAllCards()
+	deck := models.NewDeck(allCards)
+	InMemoryStorage[deck.Deck_ID] = &deck
+	return deck
+}
+
+func TestPOSTNewCompleteDeck(t *testing.T) {
+	application := &application{}
+	request, _ := http.NewRequest("POST", "/api/v1/decks", nil)
+	response := httptest.NewRecorder()
+	params := httprouter.Params{}
+	application.createDeck(response, request, params)
+	minimalDeck := models.MinimalDeck{}
+	err := json.NewDecoder(response.Body).Decode(&minimalDeck)
+	if err != nil {
+		t.Errorf("Error decoding response body: %v", err)
+	}
+
+	if minimalDeck.Deck_ID == "" {
+		t.Errorf("Deck ID is empty")
+	}
+	if minimalDeck.Shuffled != false {
+		t.Errorf("Deck is shuffled")
+	}
+	if minimalDeck.Remaining != 52 {
+		t.Errorf("Deck does not contain 52 cards")
+	}
+}
+
+func TestPOSTNewShuffledDeck(t *testing.T) {
+	application := &application{}
+	request, _ := http.NewRequest("POST", "/api/v1/decks?shuffled=true", nil)
+	response := httptest.NewRecorder()
+	params := httprouter.Params{}
+	application.createDeck(response, request, params)
+	minimalDeck := models.MinimalDeck{}
+	err := json.NewDecoder(response.Body).Decode(&minimalDeck)
+	if err != nil {
+		t.Errorf("Error decoding response body: %v", err)
+	}
+
+	if minimalDeck.Shuffled != true {
+		t.Errorf("Deck is not shuffled")
+	}
+}
+
+func TestPOSTNewDeckWithCustomCards(t *testing.T) {
+	application := &application{}
+	request, _ := http.NewRequest("POST", "/api/v1/decks?cards=4S,8S,QS,JD,KH,AD", nil)
+	response := httptest.NewRecorder()
+	params := httprouter.Params{}
+	application.createDeck(response, request, params)
+	minimalDeck := models.MinimalDeck{}
+	err := json.NewDecoder(response.Body).Decode(&minimalDeck)
+	if err != nil {
+		t.Errorf("Error decoding response body: %v", err)
+	}
+
+	if minimalDeck.Remaining != 6 {
+		t.Errorf("Deck does not contain 6 cards as expected.")
+	}
+}
+
+func TestGETDeck(t *testing.T) {
+	newDeck := createNewCompleteDeck()
+	application := &application{}
+	// GET the created deck
+	request, _ := http.NewRequest("GET", "api/v1/decks/:uuid", nil)
+	response := httptest.NewRecorder()
+	params := httprouter.Params{
+		httprouter.Param{
+			Key:   "uuid",
+			Value: newDeck.Deck_ID,
+		}}
+	application.getDeck(response, request, params)
+	deck := models.Deck{}
+	err := json.NewDecoder(response.Body).Decode(&deck)
+	if err != nil {
+		t.Errorf("Error decoding response body: %v", err)
+	}
+
+	// Validate the deck
+	if deck.Deck_ID != newDeck.Deck_ID {
+		t.Errorf("Deck ID is not the same as expected.")
+	}
+	if deck.Shuffled != newDeck.Shuffled {
+		t.Errorf("Deck is shuffled")
+	}
+	if deck.Remaining != newDeck.Remaining {
+		t.Errorf("Deck does not contain the expected number of cards")
+	}
+	if len(deck.Cards) != 52 {
+		t.Errorf("Deck does not contain 52 cards")
+	}
+}
+
+func TestGETDeckNotFound(t *testing.T) {
+	application := &application{}
+	request, _ := http.NewRequest("GET", "/api/v1/decks/some_id", nil)
+	response := httptest.NewRecorder()
+	params := httprouter.Params{}
+	application.getDeck(response, request, params)
+	if response.Code != http.StatusNotFound {
+		t.Errorf("Expected status code %v, got %v", http.StatusNotFound, response.Code)
+	}
+}
+
+func TestPOSTDrawCardFromDeck(t *testing.T) {
+	newDeck := createNewCompleteDeck()
+	application := &application{}
+	// Draw a card from the deck
+	request, _ := http.NewRequest("POST", "/api/v1/decks/:uuid/draw?count=12", nil)
+	response := httptest.NewRecorder()
+	params := httprouter.Params{
+		httprouter.Param{
+			Key:   "uuid",
+			Value: newDeck.Deck_ID,
+		}}
+	application.drawCardFromDeck(response, request, params)
+	deckCards := models.DeckCards{}
+	err := json.NewDecoder(response.Body).Decode(&deckCards)
+	if err != nil {
+		t.Errorf("Error decoding response body: %v", err)
+	}
+	if len(deckCards.Cards) != 12 {
+		t.Errorf("Number of drawn cards is incorrect. Expected %v, got %v", 12, len(deckCards.Cards))
+	}
+}
+
+func TestPOSTDrawCardFromDeckCorrectRemainingNumber(t *testing.T) {
+	newDeck := createNewCompleteDeck()
+	application := &application{}
+	// Draw a card from the deck
+	request, _ := http.NewRequest("POST", "/api/v1/decks/:uuid/draw?count=12", nil)
+	response := httptest.NewRecorder()
+	params := httprouter.Params{
+		httprouter.Param{
+			Key:   "uuid",
+			Value: newDeck.Deck_ID,
+		}}
+	application.drawCardFromDeck(response, request, params)
+	inMemoryDeck := InMemoryStorage[newDeck.Deck_ID]
+	if inMemoryDeck.Remaining != 40 {
+		t.Errorf("Remaining number is incorrect. Expected %v, got %v", 40, inMemoryDeck.Remaining)
+	}
+}
